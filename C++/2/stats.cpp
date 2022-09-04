64fde7ec22c497d704c340aec0f25f3820116832
Kinfrost <shautsou.d71@gmail.com>
2022-09-04 15:20:01 +0300

    Абоба

---
+++ C++/2/stats.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+int score;
+double  distance;
+char playAgain;
+bool shieldsUp;
+short lives, aliensKilled;
+score = 0;
+distance = 1200.76;
+playAgain = 'y';
+shieldsUp = true;
+lives =3;
+aliensKilled=10;
+double engineTemp =6572.82;
+std::cout <<"score "<< score << std::endl;
+std::cout <<"distance "<< distance << std::endl;
+std::cout <<"playAgain "<< playAgain << std::endl;
+std::cout <<"lives "<< lives << std::endl;
+std::cout <<"aliensKilled "<< aliensKilled << std::endl;
+std::cout <<"engineTemp "<< aliensKilled << std::endl;
+
+
+}
